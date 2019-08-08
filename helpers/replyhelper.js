22396d95077eaced94d3aef94b141249f3e44e90
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-08-08 15:17:02 +0530

    Add files via upload

---
+++ helpers/replyhelper.js
@@ -0,0 +1,33 @@
+var dbTransactions = require('../db/session');
+
+var errors = require('../helpers/errorstousers');
+var success = require('../helpers/successtousers');
+
+function prepareReply(incomingMessage, data) {
+    let reply = {
+        module: incomingMessage.module,
+        event: incomingMessage.event,
+        messageid: incomingMessage.messageid,
+        data: data
+    }
+    return reply;
+}
+
+module.exports = {
+
+    prepareSuccess: async (message, replyData) => {
+        let reply = prepareReply(message, replyData);
+        return success.sendData(reply);
+    },
+
+    prepareError: async (message, dbsession, error) => {
+        if(dbsession)
+            await dbTransactions.abortTransaction(dbsession);
+
+        let replyData = {
+            status: error
+        }
+        let reply = prepareReply(message, replyData);
+        return errors.sendData(reply);
+    }
+}
