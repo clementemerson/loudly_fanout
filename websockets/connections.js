f08b8ac1d3f9801cf5e3bca7aea1fe8f5f6b9733
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-08-08 15:18:08 +0530

    Create connections.js

---
+++ websockets/connections.js
@@ -0,0 +1,14 @@
+let connections = new Map();
+
+module.exports = {
+    getConnections: () => {
+        return connections;
+    },
+
+    inform: async (listOfUsers, data) => {
+        listOfUsers.forEach(eachUser => {
+            if (connections.get(eachUser.user_id))
+                connections.get(eachUser.user_id).send(JSON.stringify(data));
+        });
+    },
+}
