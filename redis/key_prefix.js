0dded5ddcf309de01780693a5ebded728d40ca67
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-08-08 15:17:22 +0530

    Create key_prefix.js

---
+++ redis/key_prefix.js
@@ -0,0 +1,11 @@
+module.exports = {
+    groupUpdate: 'GroupUpdate:',
+    pollResultUpdate: 'PollUpdate:',
+
+    groupUsers: 'GroupUsers:',
+    pollVotedUsers: 'PollVotedUsers:',
+    pollInGroups: 'pollInGroups:',
+    pollsOfGroup: 'pollsOfGroup:',
+
+    phoneNumber: 'phoneNo:',
+};
